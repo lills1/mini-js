f9708b73d54004143bdec7d91bf32d5d7e7d5bac
lills1 <88222873+lills1@users.noreply.github.com>
2021-09-02 12:16:20 +1000

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,32 @@
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "300px";
+
+});
+function changeColor(){
+
+
+document.getElementById("box").style.background="blue"; 
+
+    document.getElementById("button2").Value;
+}
+
+function changeOpacity(){
+
+
+    document.getElementById("box").style.opacity="0.3"; 
+
+     }
+
+    function reset(){   
+        document.getElementById("box").style="height:150px; width:150px; background-color:orange; margin:25px";
+        
+    }
+    function borderRadius(){
+    document.getElementById("box").style.borderRadius="25px";
+    }
+
+function filter(){
+
+    document.getElementById("box").style.filter="blur(25px)";
+}
